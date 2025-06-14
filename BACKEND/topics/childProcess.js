c630ff8da25a2a31ab4de1b1d9e01b871a685fec
Aditya Kumar <adityasng420.ak@gmail.com>
2025-06-14 18:35:51 +0530

    updated on 14-6-2025

--- BACKEND/topics/childProcess.js
+++
@@ -1,3 +0,0 @@
-const cp = require("node:child_process");
-
-cp.exec("start mspaint");
--- BACKEND/topics/helloWorld.js
+++
@@ -1,7 +0,0 @@
-console.log("hello world");
-
-const gUser = (username) => {
-    console.log(`welcome ${username}`)
-}
-
-gUser("Aditya");
